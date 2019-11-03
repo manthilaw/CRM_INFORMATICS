2e1d200fe361c192b4c9c88142705997c315ee08
manthilaw <manthilaweerasinghe@gmail.com>
2019-11-03 20:49:26 +0530

    scenes added

--- src/crm/informatics/Main.java
+++ src/crm/informatics/Main.java
@@ -11,13 +11,12 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../../resources/layout/login.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/layout/crm_call_desk_home.fxml"));
+        primaryStage.setTitle("Informatics CRM");
         primaryStage.setScene(new Scene(root));
-        primaryStage.initStyle(StageStyle.UNDECORATED);
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
